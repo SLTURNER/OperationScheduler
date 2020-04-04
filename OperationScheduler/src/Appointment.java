15fd1fae1df210a7c77ed6fc03ecf72166ad5ae2
saturner469510 <55389310+saturner469510@users.noreply.github.com>
2020-04-04 12:04:11 +0200

    Added Appointment ID

--- OperationScheduler/src/Appointment.java
+++ OperationScheduler/src/Appointment.java
@@ -2,49 +2,28 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 
 
-
+/**
+ * Class to hold information about an appointment
+ * @author Mohsin Raza
+ * @version 30/03/20
+ *
+ */
 public class Appointment {
 	
+	//instance variables
 	private Date start;
 	private int duration;
 	private Date end;
 	private String description;
 	private String location;
+	private SimpleDateFormat form = new SimpleDateFormat("dd/MM/yy HH:mm");
 	private int id;
-	private boolean hidden;
 	
-	private SimpleDateFormat form = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
-	
-	public Appointment(String date,  int duration)
-	{
-		start = new Date();
-		end = new Date();
-		
-		try 
-		{
-			start = form.parse(date);
-		}
-		catch(Exception e)
-		{
-			System.out.println();
-		}
-		
-		this.duration = duration;
-		
-		endSlotCal(start);
-		
-		description = "";
-		location = "";
-	
-		
-	}
 	
 	/**
-	 * Alternative constructor for where Description and location are known
-	 * @param date    Date as String to be parsed to date
-	 * @param duration    Duration of appointment
-	 * @param description
-	 * @param location
+	 * Constructor
+	 * @param date - the start date and time of appointment
+	 * @param duration - length of appointment in minutes
 	 */
 	public Appointment(String date,  int duration, String description, String location)
 	{
@@ -62,104 +41,119 @@ public class Appointment {
 		
 		this.duration = duration;
 		
-		endSlotCal(start);
+		end = calculateEnd(start);
 		
 		this.description = description;
 		this.location = location;
-	
-		
 	}
+	
 	
 	/**
-	 * Will make a deep copy of the appointment
-	 * @param a    Appointment to copy
+	 * Mutator
+	 * @param description of appointment
 	 */
-	public Appointment(Appointment a) 
-	{
-		start = a.getStart();
-		duration = a.getDuration();
-		end = a.getEnd();
-		description = a.getDescription();
-		location = a.getLocation();
-		id = a.getID();
-		hidden = a.getHidden();
-		
-	}
-	
 	public void setDescription(String description)
 	{
 		this.description = description;
 	}
 	
+	
+	/**
+	 * mutator
+	 * @param location of appointment
+	 */
 	public void setLocation(String location)
 	{
 		this.location = location;
 	}
 	
+	/**
+	 * Sets ID
+	 * @param id    Integer id of appointment
+	 */
+	public void setID(int id) {
+		this.id = id;
+	}
+	
+	/**
+	 * Gets ID
+	 * @return    integer id of appointment stored in fields
+	 */
+	public int getID() {
+		return id;
+	}
+	
+	/**
+	 * accessor
+	 * @return description
+	 */
 	public String getDescription()
 	{
 		return description;
 	}
 	
+	
+	/**
+	 * accessor 
+	 * @return location 
+	 */
 	public String getLocation()
 	{
 		return location;
 	}
 	
+	
+	/**
+	 * accessor 
+	 * @return duration
+	 */
 	public int getDuration()
 	{
 		return duration;
 	}
 	
-	/**
-	 * Will get ID
-	 * @return    ID as integer
-	 */
-	public int getID() 
-	{
-		return id;
-	}
 	
 	/**
-	 * Will set the value of ID, to parameter ID
-	 * @param id    New ID as integer
+	 * Calculates the end time of the appointment
+	 * @param start - of appointment
 	 */
-	public void setID(int id) 
-	{
-		this.id = id;
-	}
-	
-	
-	private void endSlotCal(Date start)
-	
+	private Date calculateEnd(Date start)
 	{
 		long min = start.getTime();
 		end.setTime(min + (duration * 60 * 1000));
-		System.out.println(end);
+		return end;
 	}
 	
+	
+	/**
+	 * accessor 
+	 * @return start
+	 */
 	public Date getStart()
 	{
 		return start;
 	}
 	
+	
+	/**
+	 * accessor 
+	 * @return end 
+	 */
 	public Date getEnd()
 	{
 		return end;
 	}
 	
 	
+	/**
+	 * formats to ensure strings are outputted/returned correctly
+	 */
 	public String toString() 
 	{
-	    String str = "";
-	    str = str + form.format(start) + " for " + duration + " minutes";
-	    return str;
-	}
-	
-	
-	
-	
-	
-	
+	    String string = "";
+	    string = string + form.format(start) + " for " + duration + " minutes";
+	    return string;
+	 }
 
+	
 }
--- OperationScheduler/src/Diary.java
+++ OperationScheduler/src/Diary.java
@@ -1,132 +1,467 @@
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 
-
+/**
+ *@author Mohsin Raza 
+ *@version 03/04/20
+ *Class to hold and perform operations on appointments
+ */
 public class Diary {
 	
-	private String patientName;
-	
+	private String staffName;
 	ArrayList<Appointment> appointment;
-	int slot;
+	int aCount = 0;
 	
-	
-	
+	/**
+	 * Constructor
+	 * @param name - of staff
+	 */
 	public Diary(String name)
 	{
-		this.patientName = name;
-		
-		slot = 0;
+		this.staffName = name;
 	}
 	
 	
-	
-	public boolean initAppointment(Appointment appointmentSlot)
+	/**
+	 * 
+	 * @param date - start date of appointment
+	 * @param duration - duration of appointment in minutes
+	 * @param description - of appointment
+	 * @param location - of appointment
+	 * @return true if added, false otherwise
+	 */
+	public boolean addAppointment(String date, int duration, String description, String location)
 	{
-		int flag = 0;
+		//initialise start date and current date
+		Date startDate = new Date();
+		Date current = new Date();
 		
-		if(slot == 0)
+		//construct new appointment
+		Appointment holder = new Appointment(date, duration, description, location);
+		
+		//parse string into Date data type
+		startDate = dateParser(date);
+		
+		//checking if string of date input is valid
+		if(isValidDate(date))
 		{
-			appointment = new ArrayList<Appointment>();
-			appointment.add(appointmentSlot);
-			slot++;
-			
-			return true;
-		}
-		else
-		{
-			for(int i = 0; i<appointment.size(); i++)
+			//only allows duration of 5 or more
+			if(duration <5)
 			{
-				if( (appointment.get(i).getEnd().compareTo(appointmentSlot.getStart())<0))
-				{
-					flag = 1;
-				}
+				System.out.println("Please enter a duration of at least 5 minutes");
 			}
 			
-			if (flag == 1)
+			//only allows future dates
+			if(startDate.before(current))
 			{
-				appointment.add(appointmentSlot);
+				System.out.println("Please enter a date in the future");
+			}
+			
+			//Creates a new appointment arraylist if none have already been booked provided that the first two if statements are false
+			if(appointment == null && duration >=5 && startDate.after(current))
+			{	
+				holder.setID(incrementCount());
+				appointment = new ArrayList<Appointment>();
+				appointment.add(holder);
+				
 				return true;
 			}
+			//adds appointment to existing arraylist provided that there are no overlaps with existing appointments and the first two if statements are false
+			else if(appointment != null && overlapCheck(date,duration) == false && duration >=5 && startDate.after(current))
+			{	
+				holder.setID(incrementCount());
+				appointment.add(holder);
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		//returns false if the appointment cannot be booked due to invalid input
+		else
+		{
+			System.out.println("Date input invalid");
+			return false;
+		}
+	}
+	
+	/**
+	 * Returns current value and increments count by 1
+	 * @return    Current count value
+	 */
+	public int incrementCount() {
+		int count = aCount;
+		aCount = count +1;
+		return count;
+	}
+	
+	
+	/**
+	 * 
+	 * @param date - start date of appointment
+	 * @return parsed date
+	 */
+	public Date dateParser(String date)
+	{
+		Date parsedDate = new Date();
+		
+		//declaring date format to be used
+		SimpleDateFormat form = new SimpleDateFormat("dd/MM/yy HH:mm");
+		
+		//handling exceptions
+		try {
+		      parsedDate = form.parse(date);
+		    } 
+			catch (ParseException e) 
+			{
+		      System.out.println();  
+		    }
+		
+		//returning date once parsed
+		return parsedDate;
+	}
+	
+	
+	/**
+	 * 
+	 * @param start - start date of appointment
+	 * @param duration - duration of appointment in minutes
+	 * @return end date which has been calculated
+	 */
+	public Date calculateEnd(Date start, int duration)
+	{
+		Date end = new Date();
+		long min = start.getTime();
+		
+		//calculation to get end time
+		end.setTime(min + (duration * 60 * 1000));
+		
+		//return end date once calculated
+		return end;
+	}
+	
+	
+	/**
+	 * Prints the booked appointments
+	 */
+	public void printApp()
+	{
+		//check to see if there are appointments to be printed
+		if(appointment !=null)
+		{
+			//sorting appointments by date
+			sortAppointments();
+			
+			System.out.println("\tThe following appointments have been booked for " + staffName );
+				
+				//looping over all existing appointments
+				for (int i = 0;i<appointment.size();i++)
+				{
+						System.out.println("--" + appointment.get(i).getStart() + " for " +  appointment.get(i).getDuration() + " minutes at " + appointment.get(i).getLocation() + "--");
+				}
+		}
+		//if no appointments have been booked
+		else
+		{
+			System.out.println("No Appointments have been booked for " + staffName);
+		}
+	}
+	
+	
+	/**
+	 * 
+	 * @param start - start date of appointment
+	 * @param duration - duration of appointment in minutes
+	 * @return true if there is an overlap, false otherwise
+	 */
+	public boolean overlapCheck(String start, int duration)
+	{
+		//new dates
+		Date startDate = new Date();
+		startDate = dateParser(start);
+		
+		//dates to be compared against
+		Date primeStart = new Date();
+		Date primeEnd = new Date();
+		
+		//counting clashes
+		int counter = 0;
+		
+		Date endDate = new Date();
+		endDate = calculateEnd(startDate, duration);
+		
+		//checking if there are appointments that may be overlapped
+		if(appointment !=null)
+		{
+			for(int i = 0;i<appointment.size();i++)
+			{
+				//ensuring all existing dates in the arraylist are checked against
+				primeStart = appointment.get(i).getStart();
+				primeEnd = appointment.get(i).getEnd();
+			
+				if(startDate.before(primeEnd) && primeStart.before(endDate))
+				{
+					//counter increases if there are any clashes
+					counter++;
+				}
+			}
 		}
 		
+		//if any clashes has occurred
+		if (counter >0)
+		{
+			System.out.println("Clash detected. Appointment not booked.");
+			return true;
+		}
+		//if no clashes have occurred
+		else
+		{
+			return false;
+		}
+	}
+	
+	
+	/**
+	 * 
+	 * @param start date of appointment
+	 * @return true if the appointment has been deleted, false otherwise
+	 */
+	public boolean deleteAppointment(String date) 
+	{
+	    Date date01 = new Date();
+	    date01= dateParser(date);
+	    
+	    //preventing null pointer exceptions
+	    if(appointment !=null)
+	    {
+		    for (int i = 0; i < appointment.size(); i++) 
+		    {
+		    	/*if the start date searched matches one in the arraylist we can assume its the appointment
+		    	the user wants deleted as only one appointment can start at any given time*/
+		    	if (date01.equals(appointment.get(i).getStart())) 
+		    	{
+		    		//removal of the appointment found
+		    		appointment.remove(i);
+		    		return true;
+		    	}
+		    }
+	    }
+	    else
+	    {
+	    	return false;
+	    }
 		return false;
 	}
 	
 	
-	public boolean bookAppointment(String description, String location, String date, int duration)
+	/**
+	 * 
+	 * @param date - start date of appointment
+	 * @param newDescription - description to replace of old one
+	 * @return true if description has been edited successfully
+	 */
+	public boolean editDescription(String date, String newDescription) 
 	{
-		int flag = 0;
-		
-		Date date01 = new Date();
-		
-		SimpleDateFormat form = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
-		
-		try 
-		{
-			date01 = form.parse(date);
-		}
-		catch(Exception e)
-		{
-			System.out.println();
-		}
-		
-		int temp = 0;
-		
-		for (int i =0; i< appointment.size(); i++)
-		{
-			if ( date01.equals(appointment.get(i).getStart())) {
-		        //System.out.print("true");
-		        temp = i;
-		        flag = 1;
-		      }
-		}
-		
-		if (flag == 1) {
-		      if (appointment.get(temp).getDescription().equals("")) {
-		        appointment.get(temp).setDescription(description);
-		        appointment.get(temp).setLocation(location);
-		        return true;
-		      } else {
-		        return false;
-		      }
-		    }
-		    return false;
-	}
-	
-	
-	public boolean deleteAppointment(String date) {
 	    Date date01 = new Date();
-	    SimpleDateFormat form = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
-	    try {
-	      date01 = form.parse(date);
-	    } catch (Exception e) {
-	      System.out.println();  
+	    date01= dateParser(date);
+	    
+	    if(appointment !=null)
+	    {	//going through all the appointments
+		    for (int i = 0; i < appointment.size(); i++) 
+		    {
+		    	/*if the start date searched matches one in the arraylist we can assume its the appointment
+		    	the user wants as only one appointment can start at any given time*/
+		    	if (date01.equals(appointment.get(i).getStart())) 
+		    	{
+		    		appointment.get(i).setDescription(newDescription);;
+		    		return true;
+		    	}
+		    }
 	    }
-	    for (int i = 0; i < appointment.size(); i++) {
-	      if (date01.equals(appointment.get(i).getStart())) {
-	        appointment.get(i).setDescription("");
-	        appointment.get(i).setLocation("");
-	        return true;
-	      }
+	    else
+	    {
+	    	return false;
 	    }
-	    return false;
-	}
+		return false;
+    }
 	
 	
-	public void displayBookedAppointments()
+	/**
+	 * 
+	 * @param date - start date of appointment to be edited
+	 * @param newLocation - new location to be input
+	 * @return true if edit is successful, false otherwise
+	 */
+	public boolean editLocation(String date, String newLocation) 
 	{
-				
-		for (int i = 0;i<appointment.size();i++)
-		{
-			if(!"".equals(appointment.get(i).getLocation()) && !"".equals(appointment.get(i).getDescription()))
-			{
-			System.out.println(appointment.get(i).getStart() + " for " +  appointment.get(i).getDuration() + " minutes at " + appointment.get(i).getLocation());
-			}
-			}
+	    Date date01 = new Date();
+	    date01= dateParser(date);
+	    
+	    for (int i = 0; i < appointment.size(); i++) 
+	    {
+	     
+	    	if (date01.equals(appointment.get(i).getStart())) 
+	    	{
+	    		appointment.get(i).setLocation(newLocation);
+	    		return true;
+	    	}
+	    }
+	    	return false;
 	}
 	
 	
-
+	/**
+	 * 
+	 * @param date - start date of appointment to be searched
+	 * @return
+	 */
+	public boolean searchAppointment(String date)
+	{
+		Date date01 = new Date();
+	    date01 = dateParser(date);
+	    int found = 0;
+	    
+	    //preventing null pointer exception
+	    if(appointment != null)
+	    {
+	    	//going through all existing appointments
+			for (int i = 0;i<appointment.size();i++)
+			{
+				/*if the start date searched matches one in the arraylist we can assume its the appointment
+		    	the user wants as only one appointment can start at any given time*/
+				if(appointment.get(i).getStart().equals(date01))
+				{
+					System.out.println("\tFound:");
+					System.out.println("--" + appointment.get(i).getStart() + " for " +  appointment.get(i).getDuration() + " minutes at " + appointment.get(i).getLocation() + "--");
+				
+					//counter increases if appointment is found
+					found++;
+				}
+			}
+	    }
+		
+		if(found == 1)
+		{
+			return true;
+		}
+		else
+		{
+			System.out.println("No appointment has been booked at that date and time");
+			return false;
+		}
+	}
+	
+	
+	/**
+	 * 
+	 * @param start - start date to search from
+	 * @param end - end date to search to
+	 */
+	public void searchByRange(String start, String end)
+	{
+		Date dateStart = new Date();
+		Date dateEnd = new Date();
+	    
+	    dateStart = dateParser(start);
+	    dateEnd = dateParser(end);
+	    
+	    //preventing null pointer exception and checking that the dates are entered in the correct ordered
+	    if(appointment !=null && dateEnd.after(dateStart))
+	    {
+	    	System.out.println();
+	    	System.out.println("Appointments from " + start + " to " + end);
+	    	
+	    	//going through all appointments
+	    	for (int i = 0;i<appointment.size();i++)
+			{
+	    		//printing appointments only in specified range
+				if(appointment.get(i).getStart().after(dateStart) && appointment.get(i).getEnd().before(dateEnd))
+				{
+					System.out.println("--" + appointment.get(i).getStart() + " for " +  appointment.get(i).getDuration() + " minutes at " + appointment.get(i).getLocation() + "--");
+				}
+			}
+	    }
+	    
+	    //if there are no appointments
+	    if(appointment == null )
+	    {
+	    	System.out.println("There are no appointments to look through");
+	    }
+	    //if the dates are incorrect order
+	    else if(dateStart.after(dateEnd))
+	    {
+	    	System.out.println("The start date must come before the end date");
+	    }
+	}
+	
+	
+	/**
+	 * Wrapper method to sort appointments by date
+	 */
+	public void sortAppointments()
+	{
+		if(appointment == null)
+		{
+			//do nothing if appointment is null
+			//preventing null pointer exceptions
+		}
+		else
+		{
+			//sort the arraylist by date
+			Collections.sort(appointment,byDate);
+		}
+	}
+	
+	
+	/**
+	 * Checks if the string entered can be parsed
+	 * @param date - string of date to be parsed
+	 * @return true if the date can be parsed, false otherwise
+	 */
+	public boolean isValidDate(String date) 
+	{
+		//defining format of date to be entered
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        //dateFormat.setLenient(true);
+        
+        //handling invalid input exceptions
+        try 
+        {
+        	//parsing string date and trimming string date, i.e. getting rid of surrounding spaces
+            dateFormat.parse(date.trim());
+        } 
+        catch (ParseException e) 
+        {
+        	//if string date cannot be parsed
+            return false;
+        }
+        return true;
+    }
+	
+	
+	/**
+	 * Comparator to aid in sorting arraylist of appointments by date
+	 * 	Sourced from stack overflow
+	 */
+	static final Comparator<Appointment> byDate = new Comparator<Appointment>() 
+	{
+	   /**
+	    * method to sort arraylist of appointments
+	    */
+	    public int compare(Appointment order01, Appointment order02) {
+	        
+	    	Date date01 = new Date();
+	        Date date02 = new Date();
+	        
+            date01 = order01.getStart();
+            date02 = order02.getStart();
+	        	        
+	       return (date01.getTime() > date02.getTime() ? 1 : -1);     
+	    }
+    };
 }
